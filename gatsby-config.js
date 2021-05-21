a67ca8c9c9a856ebf346c8765f723a5bf59197cf
Yoseph Radding <yoseph@shuttl.io>
2021-05-21 11:39:24 -0400

    Philosophy of arbor

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Document by Code Bushi`,
-    name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `ArborLang`,
+    name: `ArborLang`,
+    siteUrl: `https://arborlang.dev`,
+    description: `ArborLang is a modern language to build incredible and portable applications.`,
     social: [
       {
         name: `github`,
-        url: `https://github.com/codebushi/gatsby-theme-document`
+        url: `https://github.com/arborlang`
       },
-      {
-        name: `twitter`,
-        url: `https://twitter.com/HuntaroSan`
-      }
+
     ],
     sidebarConfig: {
       forcedNavOrder: ["/introduction", "/codeblock"],
